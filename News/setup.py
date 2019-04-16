313c13d0c4506648670bf5b0577a7ca82699f4e6
yuanshi <yshivera@gmail.com>
2019-04-15 23:19:42 -0400

    extract BBC news

--- News/setup.py
+++ News/setup.py
@@ -2,10 +2,11 @@
 # pip install newsapi first if you have not done already
 import json
 import newsapi
+import requests
 api_key = '3014e68bd7bd4196903e79989e41a196'
 
-from newsapi.articles import Articles
-a = Articles(API_KEY=api_key)
-data = a.get(source = "bbc-news", sort_by = 'top')
-for info in data['articles']:
+r = requests.get ('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey='+api_key)
+
+res = r.json()
+for info in res['articles']:
     print(info['title'])
