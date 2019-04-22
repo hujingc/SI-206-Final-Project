a81c62d2301f1041fb7d50098fbd491225d07371
hujingc <42220872+hujingc@users.noreply.github.com>
2019-04-22 00:38:08 -0400

    work

--- NYT/data_retrieval.py
+++ NYT/data_retrieval.py
@@ -6,12 +6,39 @@ import requests
 
 APISecret='zkt6V2HOt9XyBL0G'
 api = 'KIBl6ffb7lJrM6UAdpiOwAms0ppqvNf2'
-
-r=requests.get('https://api.nytimes.com/svc/topstories/v2/science.json?api-key='+api)
-
+total=0
+# subjects=['arts', 'automobiles', 'books', 'business', 'fashion',\
+#      'food', 'health', 'home', 'insider', 'magazine', \
+#         'movies', 'national', 'opinion', 'politics',\
+#     'realestate',  'sports', 'sundayreview','theater', 'tmagazine',\
+#          'travel']
+#          'world','science', 'technology', 
+r=requests.get('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key='+api+'?limit=20')
 res=r.json()
+dumped_json_cache = json.dumps(res)
+fw = open('test.json',"w")
+fw.write(dumped_json_cache)
+fw.close()
 for result in res['results']:
+    total+=1
     print(result['title'])
+print(total)
+
+conn = sqlite3.connect('news.sqlite')
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS NYT (section TEXT, title TEXT, author TEXT, time_posted TIMESTAMP, news_text TEXT)')
+for news in res['results']:
+    for x in range(20):
+        _title = news['title']
+        _author = news['byline'][3:]
+        _published = news['created_date']
+        _section = news['section']
+        cur.execute('INSERT INTO NYT (title, author, published, section) VALUES (?, ?, ?, ?, ?)',
+                 (_title, _author, _published, _section))
+conn.commit()
+#  Use the database connection to commit the changes to the database
+conn.commit()
+
 
 # SELECT * FROM table_name; select all attributes(columns) from the table → list of tuples basically
 # SELECT column1, colunm2 FROM table_name; select columns1 and column2 from table
