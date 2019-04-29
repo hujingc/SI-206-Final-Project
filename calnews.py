e50dc70e2f2101b644813f02b7075088b8a027bb
yuanshi <yshivera@gmail.com>
2019-04-29 18:54:37 -0400

    Finished vis and cal

--- calnews.py
+++ calnews.py
@@ -11,15 +11,7 @@ for row in cur:
 xvals = ["having no author names", "having author names"]
 yvals = [row[0],(100-row[0])]
 
-
-# generate a dictionary that counts the number of news from different media
-cur.execute("SELECT * FROM News GROUP BY subject HAVING COUNT(news_name) > 1")
-media_dict={}
-for row in cur:
-    media = row[0]
-    media_dict[media] = media_dict.get(media, 0) + 1
-print(media_dict)
-print(row)
+# bar chart
 plt.bar(xvals, yvals, align = "center", color = ["skyblue", "cadetblue"])
 plt.ylabel("Count of News")
 plt.xlabel("Anomity of authors")
@@ -27,21 +19,50 @@ plt.title("Number of anonymous news among 100 news")
 plt.savefig("bar.png")
 plt.show()
 
+
 # generate a dictionary that counts the number of news from different media
-cur.execute("SELECT news_name, count(news_name) as num FROM News WHERE subject == business ORDER BY num DESC LIMIT 3")
+cur.execute("SELECT * FROM News")
+media_dict={}
 for row in cur:
-    print(row[0])
-    print(row[1])
+    media = row[0]
+    media_dict[media] = media_dict.get(media, 0) + 1
+print(media_dict)
 
-# use pie chart to show the distribution
-"""
-labels = ['Mon', 'Tue', 'Wed', 'Thu','Fri','Sat','Sun']
-yvals = [day_dict['Mon'], day_dict['Tue'], day_dict['Wed'], day_dict['Thu'], day_dict['Fri'], day_dict['Sat'], day_dict['Sun']]
-
-plt.bar(xvals, yvals, align = "center", color = ["red", "blue", "yellow", "green", "black", "grey", "orange"])
-plt.ylabel("Count of days")
-plt.xlabel("Days of the week")
-plt.title("Number of tweets on different days of the week")
-plt.savefig("bar.png")
+# draw the top 5 media sources
+cur.execute("SELECT news_name, count(news_name) as num FROM News GROUP BY news_name HAVING num > 1 ORDER BY num DESC LIMIT 5")
+for row in cur:
+    print(row)
+xvals = ["CNN", "Youtube.com", "Gizmodo.com", "Espn.com", "Fox News"]
+yvals = [7,7,4,3,3]
+plt.bar(xvals, yvals, align = "center", color = ["#ba68c8", "#ba68c8", "#ce93d8", "#e1bee7", "#f3e5f5"])
+plt.ylabel("Number of Articles")
+plt.xlabel("Media Name")
+plt.title("Top 5 media sources")
+plt.savefig("bar2.png")
 plt.show()
-"""
+
+
+# generate top 3 media for business articles
+cur.execute("SELECT news_name, count(news_name) as num FROM News GROUP BY news_name HAVING subject == 'business' ORDER BY num DESC LIMIT 3")
+for row in cur:
+    print(row)
+
+# generate top 3 media for sports articles
+cur.execute("SELECT news_name, count(news_name) as num FROM News GROUP BY news_name HAVING subject == 'sports' ORDER BY num DESC LIMIT 3")
+for row in cur:
+    print(row)
+
+# generate top 3 media for entertainment articles
+cur.execute("SELECT news_name, count(news_name) as num FROM News GROUP BY news_name HAVING subject == 'entertainment' ORDER BY num DESC LIMIT 3")
+for row in cur:
+    print(row)
+
+# generate top 3 media for science articles
+cur.execute("SELECT news_name, count(news_name) as num FROM News GROUP BY news_name HAVING subject == 'science' ORDER BY num DESC LIMIT 3")
+for row in cur:
+    print(row)
+
+# generate top 3 media for technology articles
+cur.execute("SELECT news_name, count(news_name) as num FROM News GROUP BY news_name HAVING subject == 'technology' ORDER BY num DESC LIMIT 3")
+for row in cur:
+    print(row)
